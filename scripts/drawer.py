0ea64350c2d7ace4f2370976c314ce3dc01d4dde
mruggia <marco.ruggia@gmail.com>
2021-10-15 17:18:27 +0200

    upgrade to python3

--- scripts/drawer.py
+++ scripts/drawer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy
 from std_msgs.msg import Bool
@@ -7,12 +7,12 @@ import serial
 #callback for draw topic
 def draw_callback(draw):
     #forward draw command to servo
-    ret = serial.write(chr(draw.data))
+    ret = serial.write(str(int(draw.data)).encode())
 
 #callback for drawer node shutdown
 def shutdown():
     #return marker to default on
-    serial.write("1")
+    serial.write(b'1')
 
 #program entry point
 def main():
@@ -27,7 +27,7 @@ def main():
     #connect to marker
     global serial; serial = serial.Serial(device)
     #set marker to default off
-    serial.write("0")
+    serial.write(b'0')
 
     #run until node is shut down
     rospy.spin()
--- scripts/plate.py
+++ scripts/plate.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
+import math
 import rospy
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Pose2D, Pose
-from tf.transformations import euler_from_quaternion
 
 #program entry point
 def main():
@@ -23,7 +23,7 @@ def main():
 
         #get drone yaw
         odom_drone_quat = odom_drone.orientation
-        odom_drone_rpy = euler_from_quaternion([odom_drone_quat.x,odom_drone_quat.y,odom_drone_quat.z,odom_drone_quat.w])
+        odom_drone_rpy = euler_from_quaternion(odom_drone_quat.x,odom_drone_quat.y,odom_drone_quat.z,odom_drone_quat.w)
         odom_drone_yaw = odom_drone_rpy[2]
 
         #calculate plate odometry
@@ -43,6 +43,24 @@ odom_drone = Pose();
 odom_drone.orientation.w = 1.0
 def callback_odom_drone(odom_drone_new):
     global odom_drone; odom_drone = odom_drone_new.pose.pose
+
+#calculate euler angles from quaternion
+def euler_from_quaternion(x, y, z, w):
+
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    roll_x = math.atan2(t0, t1)
+
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    pitch_y = math.asin(t2)
+
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    yaw_z = math.atan2(t3, t4)
+
+    return roll_x, pitch_y, yaw_z # in radians
 
 
 if __name__ == '__main__':
